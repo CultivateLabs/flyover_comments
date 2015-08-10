5e2c0a1eeaf0fc194366922eb3c336d0db5107e6
Jack Schultz <jackschultz23@gmail.com>
2015-08-10 13:07:37 -0500

    adding check to see if flagged is set

--- spec/features/flags_spec.rb
+++ spec/features/flags_spec.rb
@@ -35,6 +35,7 @@ RSpec.feature "Flags" do
     flag = FlyoverComments::Flag.last
     expect(flag.comment_id).to eq(comment_to_flag.id)
     expect(flag.user).to eq(current_user)
+    expect(flag.flagged).to eq(true)
 
   end
 
