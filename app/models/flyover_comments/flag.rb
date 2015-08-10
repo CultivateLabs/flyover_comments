14bfa00c96825fa32aa3aa4c11a10eb20fda20df
Jack Schultz <jackschultz23@gmail.com>
2015-08-10 13:04:50 -0500

    after create to set flagged to true

--- app/models/flyover_comments/flag.rb
+++ app/models/flyover_comments/flag.rb
@@ -5,7 +5,17 @@ module FlyoverComments
 
     validates :comment_id, presence: true, uniqueness: {scope: "#{FlyoverComments.user_class_underscore}_id"}
     validates "#{FlyoverComments.user_class_underscore}_id", presence: true
-    
+
     delegate :user, to: :comment, prefix: true, allow_nil: true
+
+    after_create: set_flag
+
+
+    def set_flag
+      if !self.approved?
+        update_attribute(:flagged, true)
+      end
+    end
+
   end
 end
