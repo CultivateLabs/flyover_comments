823efd7f6ecc8d29adba280f81e63486f5673360
Jack Schultz <jackschultz23@gmail.com>
2015-08-21 14:10:12 -0500

    adding target = blank

--- lib/flyover_comments/link_parsing.rb
+++ lib/flyover_comments/link_parsing.rb
@@ -1,24 +1,24 @@
 module FlyoverComments
   module LinkParsing
-    
+
     LINK_REGEX = /((?:http:\/\/|https:\/\/|www.)\S+)/
-    
+
     def parse_links(field)
       content = send(field)
       return [] if content.blank?
       content.scan(LINK_REGEX).flatten
     end
-    
+
     def contains_links?(field)
       !parse_links(field).empty?
     end
-    
+
     def add_html_tags_to_detected_links(value)
-      value.gsub(LINK_REGEX) do |match| 
+      value.gsub(LINK_REGEX) do |match|
         href = match.include?("://") ? match : "http://#{match}"
-        "<a href=\"#{href}\">#{match}</a>"
+        "<a target=\"_blank\" href=\"#{href}\">#{match}</a>"
       end
     end
-    
+
   end
 end
