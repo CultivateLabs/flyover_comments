51ebfe2815581c995bba11ad03a1907d13ddb666
Ben Roesch <bcroesch@gmail.com>
2015-09-21 09:29:27 -0500

    Fixes for kaminari pagination

--- config/initializers/kaminari_engine_monkey_patch.rb
+++ config/initializers/kaminari_engine_monkey_patch.rb
@@ -3,9 +3,13 @@ module Kaminari
     class Tag
 
       def page_url_for(page)
-        route_set = @params.delete(:route_set) || @template.main_app
+        route_set = @params.delete(:route_set)
         arguments = @params.merge(@param_name => (page <= 1 ? nil : page), :only_path => true).symbolize_keys
-        route_set.url_for arguments
+        if route_set.nil?
+          @template.url_for(arguments)
+        else
+          route_set.url_for(arguments)
+        end
       end
 
     end
