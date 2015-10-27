beaf5919d2641cf7589c368b589c325bb50be0d9
Jack Schultz <jackschultz23@gmail.com>
2015-10-27 15:38:12 -0500

    updates to the vote model to make it like flags

--- app/models/flyover_comments/vote.rb
+++ app/models/flyover_comments/vote.rb
@@ -1,6 +1,20 @@
 module FlyoverComments
   class Vote < ActiveRecord::Base
     belongs_to :flyover_comments_comment
-    belongs_to :user_symbol
+    belongs_to FlyoverComments.user_class_symbol, class_name: "#{FlyoverComments.user_class}", foreign_key: "#{FlyoverComments.user_class_underscore}_id"
+
+    validates :comment_id, presence: true, uniqueness: {scope: "#{FlyoverComments.user_class_underscore}_id"}
+    validates "#{FlyoverComments.user_class_underscore}_id", presence: true
+
+    delegate FlyoverComments.user_class_symbol, to: :comment, prefix: true, allow_nil: true
+
+    def _user=(val)
+      send("#{FlyoverComments.user_class_symbol}=", val)
+    end
+
+    def _user
+      send(FlyoverComments.user_class_symbol)
+    end
+
   end
 end
