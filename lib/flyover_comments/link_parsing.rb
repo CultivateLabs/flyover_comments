614422b0e4c8be959d9a9fe4b09ef42e67f96c17
Jack Schultz <jackschultz23@gmail.com>
2015-11-16 14:00:41 -0600

    new regex for detecting links.

--- lib/flyover_comments/link_parsing.rb
+++ lib/flyover_comments/link_parsing.rb
@@ -2,11 +2,12 @@ module FlyoverComments
   module LinkParsing
 
     LINK_REGEX = /((?:http:\/\/|https:\/\/|www.)\S+)/
+ASDF = /\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/?)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s\`!()\[\]{};:\'\".,<>?«»“”‘’]))/i
 
     def parse_links(field)
       content = send(field)
       return [] if content.blank?
-      content.scan(LINK_REGEX).flatten
+      content.scan(ASDF).flatten.compact
     end
 
     def contains_links?(field)
@@ -14,7 +15,7 @@ module FlyoverComments
     end
 
     def add_html_tags_to_detected_links(value)
-      value.gsub(LINK_REGEX) do |match|
+      value.gsub(ASDF) do |match|
         href = match.include?("://") ? match : "http://#{match}"
         "<a target=\"_blank\" href=\"#{href}\">#{match}</a>"
       end
