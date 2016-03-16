7b23337ce1da5aaa3c2669c570985a3a3e80da0a
Ben Golden <bengolden1@gmail.com>
2016-03-16 10:49:51 -0500

    move encoding inside config block

--- spec/dummy/config/environment.rb
+++ spec/dummy/config/environment.rb
@@ -4,5 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+config.do
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
