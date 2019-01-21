d87806ba027529461a459b1eb85fddf2fbef6ab0
Ben Golden <bengolden1@gmail.com>
2019-01-21 14:51:14 -0600

    fix association

--- lib/flyover_comments/commentable.rb
+++ lib/flyover_comments/commentable.rb
@@ -1,7 +1,7 @@
 module FlyoverComments
   module Commentable
     def flyover_commentable
-      has_many :comments, -> { where('flyover_comments.comment_type = ?', nil) }, as: :commentable, class_name: "::FlyoverComments::Comment"
+      has_many :comments, -> { where('comment_type = ?', nil) }, as: :commentable, class_name: "::FlyoverComments::Comment"
     end
   end
 end
