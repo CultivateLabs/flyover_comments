bd3e8e3e821ef9f8943b86bc156f92d087bbcc73
Ben Golden <bengolden1@gmail.com>
2019-01-21 11:09:34 -0600

    add pre-publication comments scope

--- lib/flyover_comments/commentable.rb
+++ lib/flyover_comments/commentable.rb
@@ -1,7 +1,8 @@
 module FlyoverComments
   module Commentable
     def flyover_commentable
-      has_many :comments, as: :commentable, class_name: "::FlyoverComments::Comment"
+      has_many :comments, -> { where('flyover_comments.comment_type = ?', nil) }, as: :commentable, class_name: "::FlyoverComments::Comment"
+      has_many :pre_publication_comments, -> { where('flyover_comments.comment_type = ?', "pre_publication") }, as: :commentable, class_name: "::FlyoverComments::Comment"
     end
   end
 end
